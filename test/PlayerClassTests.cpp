aaed184d4a7a7a77836acce1112fe58a92be08d3
KingElrond <troy.drescher22@gmail.com>
2023-11-20 12:02:54 -0800

    added Player class tests to runAllTests executable

--- test/PlayerClassTests.cpp
+++ test/PlayerClassTests.cpp
@@ -19,31 +19,31 @@ TEST(PlayerTest, testConstructorDeaths)
 TEST(PlayerTest, testConstructorRogue)
 {
     Player Elrond("Elrond",HUMAN,ROGUE,100,100);
-    EXPECT_EQ(Elrond.get_attack(),10);
-    EXPECT_EQ(Elrond.get_defense(),10);
+    EXPECT_EQ(Elrond.get_attack(),25);
+    EXPECT_EQ(Elrond.get_defense(),15);
     EXPECT_EQ(Elrond.get_dexterity(),20);
 }
 
 TEST(PlayerTest, testConstructorPaladin)
 {
     Player Elrond("Elrond",HUMAN,PALADIN,100,100);
-    EXPECT_EQ(Elrond.get_attack(),9);
-    EXPECT_EQ(Elrond.get_defense(),15);
-    EXPECT_EQ(Elrond.get_dexterity(),10);
+    EXPECT_EQ(Elrond.get_attack(),20);
+    EXPECT_EQ(Elrond.get_defense(),25);
+    EXPECT_EQ(Elrond.get_dexterity(),15);
 }
 
 TEST(PlayerTest, testConstructorWarlock)
 {
     Player Elrond("Elrond",HUMAN,WARLOCK,100,100);
-    EXPECT_EQ(Elrond.get_attack(),9);
-    EXPECT_EQ(Elrond.get_defense(),11);
-    EXPECT_EQ(Elrond.get_dexterity(),15);
+    EXPECT_EQ(Elrond.get_attack(),15);
+    EXPECT_EQ(Elrond.get_defense(),20);
+    EXPECT_EQ(Elrond.get_dexterity(),25);
 }
 
 TEST(PlayerTest, testConstructorForged)
 {
     Player Elrond("Elrond",DWARF,FORGED,100,100);
-    EXPECT_EQ(Elrond.get_attack(),15);
-    EXPECT_EQ(Elrond.get_defense(),15);
+    EXPECT_EQ(Elrond.get_attack(),30);
+    EXPECT_EQ(Elrond.get_defense(),30);
     EXPECT_EQ(Elrond.get_dexterity(),10);
 }
