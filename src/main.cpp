641a19eab59aab896a6084a45b3671a3cceefd94
simone07734 <93556775+simone07734@users.noreply.github.com>
2023-11-21 14:05:57 -0800

    Revert "add main and add features to GameManager"

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include "Character.h"
 #include "BattleManager.h"
-#include "GameManager.h"
 
 int main()
 {
-    IOManager iomanager(std::cin, std::cout);
-    GameManager gm(iomanager);
-    gm.startGame();
     return 0;
 }
