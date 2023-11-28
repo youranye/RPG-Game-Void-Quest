49629ce26dda93d2cd4c7b2a087f86c8695e6313
KingElrond <troy.drescher22@gmail.com>
2023-11-27 20:17:38 -0800

    removed a few extra lines from main

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,8 @@ using namespace std;
 
 int main()
 {
-    std::stringstream ss;
     IOManager manager{cin, cout};
     CharacterManager characterManager{manager};
-    Player* player = new Player("Elrond",HUMAN,ROGUE,200,100);
     SceneManager sceneManager(std::make_unique<FileSceneStore>("./resources"));
     GameManager gm(manager, characterManager, sceneManager);
     gm.startGame();
